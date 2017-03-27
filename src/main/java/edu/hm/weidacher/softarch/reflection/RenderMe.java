048a94a17b386fa1f4590a8ce38cc858888d1e3b
Simon Weidacher <simon.weidacher@posteo.de>
2017-03-27 19:04:01 +0200

    Adds annotation RenderMe.

---
+++ src/main/java/edu/hm/weidacher/softarch/reflection/RenderMe.java
@@ -0,0 +1,27 @@
+package edu.hm.weidacher.softarch.reflection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Simon Weidacher <simon.weidacher@timebay.eu>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Target({ElementType.FIELD})
+public @interface RenderMe {
+
+    String DEFAULT_RENDERING_PROVIDER = "";
+
+    /**
+     * Declares which rendering provider to use.
+     *
+     * Must be a fully qualified class name.
+     * @return n.a.
+     */
+    String with() default DEFAULT_RENDERING_PROVIDER;
+
+}
